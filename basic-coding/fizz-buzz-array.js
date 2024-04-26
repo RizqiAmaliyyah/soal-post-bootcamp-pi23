af20bdd1987c666514d9de7ffa7d7db8c504feaf
RizqiAmaliyyah <rizqia439@gmail.com>
2024-04-26 13:28:22 +0700

    first commit

---
+++ basic-coding/fizz-buzz-array.js
@@ -0,0 +1,10 @@
+// Note: Jawab dengan 2 versi, yaitu menggunakan for dan built-in function (map/filter/find/dll)
+
+/* Fizz buzz array
+- Mencari mengubah data fizz buzz
+- Buat fungsi yang menerima parameter data array
+- Kembalikan dalam bentuk boolean
+
+Contoh:
+
+*/
